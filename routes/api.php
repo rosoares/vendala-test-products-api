78ff9ad550e72edea8ae4adedf24b0287b94b31b
Rodrigo Soares <rodrigosoares8899@gmail.com>
2020-09-30 09:58:35 -0300

    Create endpoints for authentication

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'API\Auth\AuthController@login');
+    Route::post('logout', 'API\Auth\AuthController@logout');
+    Route::post('refresh', 'API\Auth\AuthController@refresh');
+    Route::post('me', 'API\Auth\AuthController@me');
+
+});
