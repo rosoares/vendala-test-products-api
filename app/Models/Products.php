7dcfa782234cd576bad47f1c72f8ca166659fd4f
Rodrigo Soares <rodrigosoares8899@gmail.com>
2020-10-01 22:19:03 -0300

    Define models for the products endpoint

---
+++ app/Models/Products.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Products extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'slug', 'created_by'];
+
+    protected $dates = ['created_by', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function createdBy()
+    {
+        return $this->hasOne(User::class, 'id', 'created_by');
+    }
+
+    public function colorVariations()
+    {
+        return $this->hasMany(ProductsVariations::class, 'product_id');
+    }
+}
---
+++ app/Models/ProductsVariations.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductsVariations extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'products_has_variations';
+
+    protected $fillable = [
+        'product_id',
+        'color_id',
+        'first_stock',
+        'available_stock',
+        'price'
+    ];
+}
